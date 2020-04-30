1deff24978bc2ef3493d6f1a20b4f62db0c4d42e
Artem Rybachuk <you@example.com>
2020-04-30 20:43:36 +0300

    url fields + lang labels

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -11,6 +11,12 @@
         'signup_fields_comment' => 'Defaults are email, password, password_confirmation',
         'login_fields' => 'Login Fields',
         'login_fields_comment' => 'Defaults are email, password',
+        'urls_section' => 'Endpoints',
+        'url_prefix' => 'Prefix',
+        'url_login' => 'Login',
+        'url_signup' => 'Signup',
+        'url_refresh' => 'Refresh',
+        'url_invalidate' => 'Invalidate',
     ],
     'settings' => [
         'page_name' => 'JWTauth settings',
